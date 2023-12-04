9452e055998b21dd2651b76d6ad404da4c695bb4
mp38590 <mp38590@gmail.com>
2023-12-04 19:49:20 +0700

    frontend 2,4

--- app/src/main/java/com/android/warmindoinspirasiindonesia/MainActivity.kt
+++ app/src/main/java/com/android/warmindoinspirasiindonesia/MainActivity.kt
@@ -46,7 +46,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
+        menuInflater.inflate(R.menu.activity_main, menu)
         return true
     }
 
