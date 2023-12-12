0618e2dc35036967b708272f318eb4ff5654d79d
AbigailMetanoia <100354595+AbigailMetanoia@users.noreply.github.com>
2023-12-12 20:30:45 +0700

    login + reg FE BE

--- app/src/main/java/com/android/warmindoinspirasiindonesia/LoginActivity.kt
+++ app/src/main/java/com/android/warmindoinspirasiindonesia/LoginActivity.kt
@@ -1,29 +1,50 @@
 package com.android.warmindoinspirasiindonesia
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.warmindoinspirasiindonesia.databinding.LoginActivityBinding
 
 class LoginActivity : AppCompatActivity() {
 
-        private lateinit var emailAddressEditText: EditText
-        private lateinit var passwordEditText: EditText
-        private lateinit var loginButton: Button
+    private lateinit var binding: LoginActivityBinding
+    private lateinit var databaseHelper: DatabaseHelper
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.login_activity)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = LoginActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            // Inisialisasi View
-            emailAddressEditText = findViewById(R.id.editTextTextEmailAddress)
-            passwordEditText = findViewById(R.id.editTextTextPassword)
-            loginButton = findViewById(R.id.btnLogin)
+        databaseHelper = DatabaseHelper(this)
 
-            // Set onClickListener untuk tombol login
-            loginButton.setOnClickListener {
-                val emailAddress = emailAddressEditText.text.toString()
-                val password = passwordEditText.text.toString()
-            }
+        binding.btnLogin.setOnClickListener{
+            val loginEmail = binding.editTextTextEmailAddress.text.toString()
+            val loginPassword = binding.editTextTextPassword.text.toString()
+
+            loginDatabase(loginEmail, loginPassword)
+
         }
+
+        binding.SignUpRedirect.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun loginDatabase(email: String, password:String){
+        val userExists = databaseHelper.readUser(email, password)
+        if (userExists){
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
