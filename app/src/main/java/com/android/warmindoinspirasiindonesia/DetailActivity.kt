e2077deda60135c20fb9bc4990766c3545425655
AbigailMetanoia <100354595+AbigailMetanoia@users.noreply.github.com>
2023-12-19 18:49:52 +0700

    backend edit status

--- app/src/main/java/com/android/warmindoinspirasiindonesia/DetailActivity.kt
+++ app/src/main/java/com/android/warmindoinspirasiindonesia/DetailActivity.kt
@@ -1,12 +1,24 @@
 package com.android.warmindoinspirasiindonesia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetailActivity : AppCompatActivity() {
 
+    private lateinit var btnUbah: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_detail)
+
+        btnUbah = findViewById(R.id.btn_ubah)
+
+        btnUbah.setOnClickListener {
+            val intent = Intent(this, EditorActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
